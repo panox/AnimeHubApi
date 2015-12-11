7f5fdd9dcb8f709ab93a97520dbbeae929e85c99
panox <panospalos@gmail.com>
2015-12-11 16:24:49 +0000

    model and database conection

--- db/animeData.js
+++ db/animeData.js
@@ -1,4 +1,13 @@
 var request = require('request-json');
+
+var mongoose = require("mongoose");
+var Anime   = require('../models/anime');
+
+// Database
+var config = require('../config/config');
+mongoose.connect(config.database);
+
+
 //data to get token
 var data = {
   grant_type    : "client_credentials",
@@ -17,20 +26,17 @@ client.post('auth/access_token', data, function(err, res, body) {
     '&type=TV' +
     '&status=Currently%20Airing'
   var getUrl = 'browse/anime' + sendClientToken + params
-
-  
-  
-  // //get all anime with url params
-  // client.get(getUrl, function(err, res, body) {
-  //   // loop request for every anime from browse response
-  //   for (var i = body.length - 1; i >= 0; i--) {
-  //     var animeId = body[i].id
-  //     var url = 'anime/' + animeId + sendClientToken
-  //     // get data for each anime by get request
-  //     client.get(url, function(err, res, body) {
-  //       console.log(body)
-  //     })
-  //   };
-  // });
+  //get all anime with url params
+  client.get(getUrl, function(err, res, body) {
+    // loop request for every anime from browse response
+    for (var i = body.length - 1; i >= 0; i--) {
+      var animeId = body[i].id
+      var url = 'anime/' + animeId + sendClientToken
+      // get data for each anime by get request
+      client.get(url, function(err, res, body) {
+        console.log(body)
+      })
+    };
+  });
 });
 
