ed09013253f49a09a51cae81cbc4ba92d6302ec9
panox <panospalos@gmail.com>
2015-12-11 15:09:56 +0000

    body parser

--- db/animeData.js
+++ db/animeData.js
@@ -1,4 +1,10 @@
+var express        = require('express');
+var bodyParser     = require('body-parser');
+var app            = express();
 var request = require('request');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var data = {
   grant_type    : "client_credentials",
@@ -10,5 +16,7 @@ var url = 'https://anilist.co/api/auth/access_token';
 
 request.post(url, { form: data }, function (error, response, body) {
     console.log(body)
+    console.log(typeof body)
+    console.log(body["access_token"])
   }
 );
