188fc92deb3db00cfdf9909eca711d9efbe1b079
panox <panospalos@gmail.com>
2015-12-11 15:46:26 +0000

    seperated params in variables

--- db/animeData.js
+++ db/animeData.js
@@ -10,8 +10,9 @@ var client = request.createClient('https://anilist.co/api/');
 
 client.post('auth/access_token', data, function(err, res, body) {
   var clientToken = body["access_token"];
-  var getUrl = 'browse/anime' + '?access_token=' + clientToken +
-  '&season=fall&sort=score-desc&type=TV&&status=Currently%20Airing'
+  var sendClientToken = 'access_token=' + clientToken
+  var sorting = '&season=fall&sort=score-desc&type=TV&&status=Currently%20Airing'
+  var getUrl = 'browse/anime?' + sendClientToken + sorting
   client.get(getUrl, function(err, res, body) {
     console.log(body);
   });
