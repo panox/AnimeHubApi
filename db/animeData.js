c6d0ea23bb04592cb723344f10a8e0a9a4b177bd
panox <panospalos@gmail.com>
2015-12-11 15:28:02 +0000

    can get one anime back

--- db/animeData.js
+++ db/animeData.js
@@ -10,4 +10,8 @@ var client = request.createClient('https://anilist.co/api/');
 
 client.post('auth/access_token', data, function(err, res, body) {
   var clientToken = body["access_token"];
+  var getUrl = 'anime/1' + '?access_token=' + clientToken
+  client.get(getUrl, function(err, res, body) {
+    console.log(body);
+  });
 });
