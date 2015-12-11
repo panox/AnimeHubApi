3665fce1dd017043da443d91c472e11a17bd0a36
panox <panospalos@gmail.com>
2015-12-11 15:37:30 +0000

    can get current season

--- db/animeData.js
+++ db/animeData.js
@@ -10,7 +10,8 @@ var client = request.createClient('https://anilist.co/api/');
 
 client.post('auth/access_token', data, function(err, res, body) {
   var clientToken = body["access_token"];
-  var getUrl = 'anime/1' + '?access_token=' + clientToken
+  var getUrl = 'browse/anime' + '?access_token=' + clientToken +
+  '&season=fall&sort=score-desc&type=TV&&status=Currently%20Airing'
   client.get(getUrl, function(err, res, body) {
     console.log(body);
   });
