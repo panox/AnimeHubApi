2ddc62f1db5f61891eab08b6fedfbbb3772c0515
panox <panospalos@gmail.com>
2015-12-11 13:06:51 +0000

    request with request module

--- db/animeData.js
+++ db/animeData.js
@@ -1,15 +1,4 @@
-var http = require('http');
-
-//The url we want is `www.nodejitsu.com:1337/`
-var options = {
-  host: 'www.nodejitsu.com',
-  path: '/',
-  //This is what changes the request to a POST request
-  method: 'POST'
-};
-
-callback = function(response) {
-  console.log(response)
-}
-
-http.request(options, callback);
+var request = require('request');
+request('https://api.doughnuts.ga/doughnuts', function (error, res) {
+  console.log(res)
+})
