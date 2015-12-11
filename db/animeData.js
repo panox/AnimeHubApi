ccc4dbd49029bb74f9d5576a275e888abc181517
panox <panospalos@gmail.com>
2015-12-11 15:02:02 +0000

    request for client token

--- db/animeData.js
+++ db/animeData.js
@@ -1,11 +1,12 @@
 var request = require('request');
 
 var data = {
-  "email":"ll@ga.co", 
-  "password":"password"
+  grant_type    : "client_credentials",
+  client_id     :  process.env.ClientID,
+  client_secret :  process.env.ClientSecret,
 }
 
-var url = 'http://localhost:3000/api/login';
+var url = 'https://anilist.co/api/auth/access_token';
 
 request.post(url, { form: data }, function (error, response, body) {
     console.log(body)
