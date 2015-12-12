961f48bf1da50f29e7b83e25c1ce3f4ce495364a
panox <panospalos@gmail.com>
2015-12-12 12:55:54 +0000

    empty methods for comments controller

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,8 +1,16 @@
 var Comment   = require('../models/comment');
 
+//create comments
+function commentsCreate(req, res){
+}
+// edit comments
+function commentUpdate(req, res){
+}
+// delete comments
+function commentDelete(req, res){
+}
 
 module.exports = {
-  commentsIndex:  commentsIndex,
   commentsCreate: commentsCreate,
   commentUpdate: commentUpdate,
   commentDelete: commentDelete
