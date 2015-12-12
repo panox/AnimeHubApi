ab72c78c96fc2f98769c4327fc010336050a8705
panox <panospalos@gmail.com>
2015-12-12 13:02:41 +0000

    comment update

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -5,6 +5,20 @@ function commentsCreate(req, res){
 }
 // edit comments
 function commentUpdate(req, res){
+  Comment.findById(req.params.id,  function(err, comment) {
+    if (err) return res.status(500).json({message: "Something went wrong!"});
+    if (!comment) return res.status(404).json({message: 'No comment found.'});
+
+    if (req.body.title) comment.title = req.body.title;
+    if (req.body.content) comment.content = req.body.content;
+    if (req.body.score) comment.score = req.body.score;
+
+    comment.save(function(err) {
+     if (err) return res.status(500).json({message: "Something went wrong!"});
+
+      res.status(201).json({message: 'Comment successfully updated.', comment: comment});
+    });
+  });
 }
 // delete comments
 function commentDelete(req, res){
