fb1f7338638fe1a130a14a6d482b49345503fd77
panox <panospalos@gmail.com>
2015-12-12 13:44:41 +0000

    comment user and anime required

--- models/comment.js
+++ models/comment.js
@@ -4,8 +4,8 @@ var CommentSchema = new mongoose.Schema({
   title: { String, required: true }
   content: { String, required: true }
   score: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  anime: { type: mongoose.Schema.Types.String, ref: 'Anime'}
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  anime: { type: mongoose.Schema.Types.String, ref: 'Anime', required: true }
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
