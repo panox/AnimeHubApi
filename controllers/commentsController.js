8a560798e4898b0bdefe3bd3aaa0f4902edc2b50
panox <panospalos@gmail.com>
2015-12-12 13:00:48 +0000

    comment delete method

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -8,6 +8,10 @@ function commentUpdate(req, res){
 }
 // delete comments
 function commentDelete(req, res){
+  Comment.findByIdAndRemove({_id: req.params.id}, function(err){
+   if (err) return res.status(404).json({message: 'Could not delete comment'});
+   res.status(200).json({message: 'Comment has been successfully deleted'});
+  });
 }
 
 module.exports = {
