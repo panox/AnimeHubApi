264f2b565d15f18dfed37b3d71dcf8032eb38785
panox <panospalos@gmail.com>
2015-12-12 12:24:55 +0000

    require anime model

--- controllers/animesController.js
+++ controllers/animesController.js
@@ -1,4 +1,20 @@
+var Anime   = require('../models/anime');
 
+//get all the users
+function usersIndex(req, res) {
+  User.find().populate('images').exec(function(err, users){
+    if (err) return res.status(404).json({message: 'No users found'});
+    res.status(200).json({ users: users });
+  });
+}
+
+//get one user
+function userShow(req, res){
+  User.findById(req.params.id).populate('images').exec( function(err, user){
+    if (err) return res.status(404).json({message: 'No user found'});
+    res.status(200).json({ user: user });
+  });
+}
 
 module.exports = {
   animesIndex:  animesIndex,
