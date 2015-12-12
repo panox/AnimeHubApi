cc66c24e0a3d57f12eb3dbc5277dc89172dc7426
panox <panospalos@gmail.com>
2015-12-12 12:52:01 +0000

    export controller methods

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,1 +1,9 @@
 var Comment   = require('../models/comment');
+
+
+module.exports = {
+  commentsIndex:  commentsIndex,
+  commentsCreate: commentsCreate,
+  commentUpdate: commentUpdate,
+  commentDelete: commentDelete
+}
