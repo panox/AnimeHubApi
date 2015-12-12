e715f4e2377a3282b7aae112daba9e1ed7c8df1e
panox <panospalos@gmail.com>
2015-12-12 13:15:21 +0000

    user reference for comment

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,8 @@ var mongoose = require("mongoose");
 var CommentSchema = new mongoose.Schema({
   title: String,
   content: String,
-  score: String
+  score: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
