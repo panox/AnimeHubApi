c570c335e61f9c28498098756e2946b4df28320d
panox <panospalos@gmail.com>
2015-12-14 10:53:14 +0000

    backup

--- db/backup.js
+++ db/backup.js
@@ -1,6 +1,6 @@
 var backup = require('mongodb-backup');
 
 backup({
-  uri: 'uri', // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase> 
+  uri: 'mongodb://localhost/animehub',
   root: __dirname
 });
