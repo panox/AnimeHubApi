7f95f090620120476de8659171233b1cad1d30a1
panox <panospalos@gmail.com>
2015-12-25 23:48:31 +0200

    changed score with number

--- models/anime.js
+++ models/anime.js
@@ -4,7 +4,7 @@ var AnimeSchema = new mongoose.Schema({
   _id: String,
   picture: String,
   title: String,
-  rating: String,
+  rating: Number,
   episodes: String,
   description: String,
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
