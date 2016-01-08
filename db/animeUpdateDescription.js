9afb438168ac88e764f0cd90a0f2cf2c550119a3
panox <panospalos@gmail.com>
2016-01-08 12:23:51 +0000

    update description uses aniToken module

--- db/animeUpdateDescription.js
+++ db/animeUpdateDescription.js
@@ -1,25 +1,13 @@
 var request = require('request-json');
 var mongoose = require("mongoose");
 var Anime   = require('../models/anime');
+var Token = require('./aniToken');
 
 // Database
 var config = require('../config/config');
 mongoose.connect(config.database);
 
-//data to get token
-var data = {
-  grant_type    : "client_credentials",
-  client_id     :  process.env.ClientID,
-  client_secret :  process.env.ClientSecret,
-};
-// client url
-var client = request.createClient('https://anilist.co/api/');
-//post to get token back
-client.post('auth/access_token', data, function(err, res, body) {
-  if (err) return console.log(err); //error
-  var clientToken = body.access_token;
-  var sendClientToken = '?access_token=' + clientToken;
-
+function updateDescriptions(sendClientToken, client) {
   Anime.find().exec(function(err, animes){
     if (err) return console.log(err); //error
     // loop through all animes in db
@@ -37,6 +25,6 @@ client.post('auth/access_token', data, function(err, res, body) {
       });
     }
   });
-});
+}
 
-console.log('done');
+Token.getAniToken(updateDescriptions);
